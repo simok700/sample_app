c6e26bab5617577f88139eb4af7bedfc40a91fa5
DAOUDI <moda10000@gmail.com>
2014-02-05 18:30:08 +0100

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -35,4 +35,16 @@ describe PagesController do
       response.should have_selector("title",:content =>"Simple App du Tutoriel Ruby on Rails | A Propos")
     end
   end
+  
+  describe "GET 'help'" do
+    it "devrait reussir" do
+      get 'help'
+      response.should be_success
+    end
+    it "devrait avoir le bon titre" do
+      get 'help'
+      response.should have_selector("title",:content =>"Simple App du Tutoriel Ruby on Rails | Aide")
+    end
+  end
+  
 end
